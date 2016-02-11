88943a1b57a5c1e717037f82e02d4f490ef9f86c
harveyl888 <harvey.lieberman@gmail.com>
2016-02-11 10:49:05 -0500

    Updated comment.

--- app.R
+++ app.R
@@ -3,6 +3,8 @@
 # The brain image was created with polygons using QGIS (http://www.qgis.org/) and saved as a shapefile.
 # The shapefile can then be read into R using maptools and treated as a map and each polygon
 # can be colored according to a scalar.
+# 
+# Brain image taken from http://www.highlands.edu/academics/divisions/scipe/biology/faculty/harnden/2121/images/brainlandmarks.jpg
 #
 
 library(shiny)
